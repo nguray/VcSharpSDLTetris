8c9a0e7e9e3f2b9e0f5fd9dfb1c736a0f779c86c
nguray <raymond.nguyenthanh@gmail.com>
2022-10-02 18:38:36 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using SDL2;
+
+namespace SfmlTetris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+			SDL.SDL_Init(SDL.SDL_INIT_VIDEO);
+
+            var window = IntPtr.Zero;
+
+            window = SDL.SDL_CreateWindow("SDL2 Tetris",SDL.SDL_WINDOWPOS_CENTERED,
+                        SDL.SDL_WINDOWPOS_CENTERED,800,600,SDL.SDL_WindowFlags.SDL_WINDOW_SHOWN);
+			
+            SDL.SDL_Event e;
+
+            bool quit = false;
+            while(!quit)
+            {
+                while (SDL.SDL_PollEvent(out e)!=0)
+                {
+                    switch(e.type)
+                    {
+                        case SDL.SDL_EventType.SDL_QUIT:
+                            quit = true;
+                            break;
+
+                    }
+                }
+            }
+
+            SDL.SDL_DestroyWindow(window);
+
+			SDL.SDL_Quit();
+            
+
+        }
+	
+	}
+}
